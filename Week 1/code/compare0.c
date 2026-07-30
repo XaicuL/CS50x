72a2e81a746d61809628f98c2ed39b860b2bf8ec
XaicuL <hyunjun050915@gmail.com>
2026-07-31 03:24:13 +0900

    add : file

---
+++ Week 1/code/compare0.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int x;
+    int y;
+
+    printf("What's x?");
+    scanf("%d", &x);
+
+
+    printf("What's y?");
+    scanf("%d", &y);
+
+    if (x < y)
+    {
+        printf("x is less than y \n");
+
+    }
+
+    return 0;
+
+}
